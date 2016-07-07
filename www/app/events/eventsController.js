950190bbde277f5b7d5ab245651561f847baf58f
offlineprogrammer <mmalaka@gmail.com>
2016-07-07 22:50:52 +0100

    Events Style

--- www/app/events/eventsController.js
+++ www/app/events/eventsController.js
@@ -5,19 +5,21 @@
         .module('countingDays')
         .controller('eventsController', EventsController);
 
-    function EventsController($scope, $state, eventsService, $stateParams, $ionicPopup, $ionicHistory,$ionicModal,googleAnalyticsService) {
+    function EventsController($scope, $state, eventsService, $stateParams, $ionicPopup, $ionicHistory, $ionicModal, googleAnalyticsService,$timeout) {
         $ionicModal.fromTemplateUrl('app/events/viewEvent.tpl.html', {
             scope: $scope
         }).then(function (modal) {
             $scope.viewEventModal = modal;
 
-           
+
         });
+
+         var selectedIndex;
 
         function init() {
             $scope.items = eventsService.get();
-          
-       };
+
+        };
 
         $scope.addEvent = function () {
             $state.go('addevent');
@@ -26,56 +28,78 @@
         $scope.showInfo = function () {
             googleAnalyticsService.trackView('showInfo');
             var prompt = $ionicPopup.show({
-                    title: 'CountingDays',
-                    cssClass: 'popup-autowidth',
-                    templateUrl: 'app/events/viewInfo.tpl.html',
-                    scope: $scope,
-                    buttons: [{
-                        text: 'Close',
-                        type: 'button-assertive'
-                    }]
-                });
-                
-                 prompt.then(function (result) {
-                   
+                title: 'CountingDays',
+                cssClass: 'popup-autowidth',
+                templateUrl: 'app/events/viewInfo.tpl.html',
+                scope: $scope,
+                buttons: [{
+                    text: 'Close',
+                    type: 'button-assertive'
+                }]
+            });
 
-                   
-                });
+            prompt.then(function (result) {
+
+
+
+            });
 
         };
 
         $scope.showEvent = function (eventItem) {
             googleAnalyticsService.trackView('showEvent');
-             $scope.viewEventModal.scope.data = {
-                    eventItem: eventItem
-                   
-                };
+            $scope.viewEventModal.scope.data = {
+                eventItem: eventItem
 
-                $scope.viewEventModal.show();
+            };
+
+            $scope.viewEventModal.show();
         };
 
         $scope.hideEvent = function () {
             var data = $scope.viewEventModal.scope.data;
 
-           
+
 
             $scope.viewEventModal.hide();
             delete $scope.viewEventModal.scope.data;
         };
 
         $scope.deleteEvent = function (eventItem) {
-           eventsService.remove(eventItem);
-           googleAnalyticsService.trackEvent('Events', 'deleteEvent',eventItem.name);
+            eventsService.remove(eventItem);
+            googleAnalyticsService.trackEvent('Events', 'deleteEvent', eventItem.name);
             $scope.items = eventsService.get();
 
             $scope.viewEventModal.hide();
             delete $scope.viewEventModal.scope.data;
         };
 
+        $scope.isSelected = function (index) {
+            return index === selectedIndex;
+        };
+
+        function resetSelection() {
+            $timeout(function () {
+                selectedIndex = -1;
+                
+            });
+        }
 
 
-       
+        $scope.select = function (index) {
+            if ($scope.isSelected(index)) {
+                resetSelection();
+            } else {
+                selectedIndex = index;
+               // scrollIntoView(event.currentTarget);
+            }
+        };
 
-    init();
+
+
+
+
+
+        init();
     }
 } ());
